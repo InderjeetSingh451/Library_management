1442055bcc4c4071c3dfc85db1f2e760e8701b18
Inderjeet <itsforourfuture542@gmail.com>
2026-02-08 02:27:05 +0530

    Update Loader.jsx

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,7 +1,10 @@
 const Loader = () => {
   return (
     <div className="flex justify-center items-center h-40">
-      <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-black"></div>
+      <div className="relative">
+        <div className="w-12 h-12 rounded-full bg-black/20 animate-ping absolute"></div>
+        <div className="w-12 h-12 rounded-full border-4 border-black border-t-transparent animate-spin"></div>
+      </div>
     </div>
   );
 };
