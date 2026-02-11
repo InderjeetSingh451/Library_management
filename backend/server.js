b5a6d102ee0639925a2b8c2c3f44df1416596842
Inderjeet <itsforourfuture542@gmail.com>
2026-02-11 14:11:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use((req,res,next)=>{
+  console.log("Request come : ", req.path);
+  next();
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/students", studentRoutes);
 app.use("/api/attendance", attendanceRoutes);
@@ -27,3 +30,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
